d36865a1b57939f47cc5c9fe7fe0e6659079d0fd
nerea <801950@unizar.es>
2022-10-09 04:02:09 -0700

    matriz

--- p1/Matrix4.h
+++ p1/Matrix4.h
@@ -10,28 +10,100 @@ class Matrix4{
 private:
     float mat[4][4];
 
+    /**
+     * @brief devuelve el determinante de la matriz
+     * 
+     * @return float 
+     */
     float det() const;
+
+    /**
+     * @brief devuelve la matriz adjunta
+     * 
+     * @return Matrix4 
+     */
     Matrix4 adjunta() const;
+
+    /**
+     * @brief devuelve la matriz tanspuesta
+     * 
+     * @return Matrix4 
+     */
     Matrix4 transpuesta() const;
 
 public:
+    /**
+     * @brief Construct a new Matrix 4 object
+     * Construye la matriz vacía (todas sus componentes son 0)
+     * 
+     */
     Matrix4(): mat{{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}} {}
+
+    /**
+     * @brief Construct a new Matrix 4 object
+     * Construye la matriz a partir de un vector 4x4
+     * 
+     * @param m 
+     */
     Matrix4(float m[4][4]);
 
+    /**
+     * @brief devuelve la suma de dos matrices
+     * 
+     * @param m matriz con la que se va a sumar
+     * @return Matrix4 
+     */
     Matrix4 operator +(const Matrix4 m) const;
 
+    /**
+     * @brief devuelve la resta de dos matrices
+     * 
+     * @param m matriz con la que se va a restar
+     * @return Matrix4 
+     */
     Matrix4 operator -(const Matrix4 m) const;
 
+    /**
+     * @brief devuelve la multiplicación de dos matrices
+     * 
+     * @param m matriz por la que se va a multiplicar
+     * @return Matrix4 
+     */
     Matrix4 operator *(const Matrix4 m) const;
+
+    /**
+     * @brief devuelve el resultado de multiplicar una matriz por un escalar
+     * 
+     * @param a escalar por el que se va a multiplicar
+     * @return Matrix4 
+     */
     Matrix4 operator *(const float a) const;
+
+    /**
+     * @brief devuelve el resultado de multiplicar una matrix 4x4 por una 
+     * matriz 4x1
+     * 
+     * @param v matriz 4x1 por la que se va a multiplicar
+     * @return vector<float> 
+     */
     vector<float> operator *(const float v[4]);
+
+    /**
+     * @brief operador de acceso a la matriz
+     * 
+     * @param index índice al que se quiere acceder
+     * @return float* 
+     */
     float* operator[](const int &index) ;
 
+    /**
+     * @brief devuelve la matriz inversa
+     * 
+     * @return Matrix4 
+     */
     Matrix4 inversa() const;
 
-   //decltype(auto) operator[](unsigned int i) { return mat[i]; }
-    //decltype(auto) operator[](unsigned int i) const { return mat[i]; }
-
+    // pretty stdout
     friend ostream& operator<<(ostream& os, const Matrix4& mat);
 };
 
