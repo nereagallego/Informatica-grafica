31dc01e158b2eb92f322601698452d70f27007c4
nerea <801950@unizar.es>
2022-10-18 13:44:05 -0700

    rgb

--- p2/RGB.h
+++ p2/RGB.h
@@ -9,14 +9,55 @@ class RGB{
     float Blue;
 
     public:
+    /**
+     * @brief Construct a new RGB object
+     * 
+     * @param red_ value of the red pixel
+     * @param green_ value of the green pixel
+     * @param blue_ value of the blue pixel
+     */
     RGB(float red_ ,float green_, float blue_);
 
+    /**
+     * @brief Get the Red object
+     * 
+     * @return float 
+     */
     float getRed();
+
+    /**
+     * @brief Get the Green object
+     * 
+     * @return float 
+     */
     float getGreen();
+    
+    /**
+     * @brief Get the Blue object
+     * 
+     * @return float 
+     */
     float getBlue();
 
+    /**
+     * @brief Set the Red object
+     * 
+     * @param Red_ 
+     */
     void setRed(float Red_);
+
+    /**
+     * @brief Set the Green object
+     * 
+     * @param Green_ 
+     */
     void setGreen(float Green_);
+
+    /**
+     * @brief Set the Blue object
+     * 
+     * @param Blue_ 
+     */
     void setBlue(float Blue_);
 };
 
