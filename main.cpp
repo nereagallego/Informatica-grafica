947ef07d638b933fb63878ff0c6519ccf9f3207b
nerea <801950@unizar.es>
2022-10-23 03:42:56 -0700

    función intersect

--- main.cpp
+++ main.cpp
@@ -13,7 +13,18 @@ int main(){
     Plano plane(d,3);
     Ray r(Direccion(1,0,0),Punto(0,0,0));
     
-    Punto p = r.intersect(plane);
-    cout << p << endl; 
+    float t = r.intersect(plane);
+    if (t != -1){
+        Punto p = r.getPunto() + r.getDireccion() * t;
+        cout << p << endl;
+    }
+
+    Esfera e(Direccion(0,2,0),Punto(3,0,0), Punto(3,1,0));
+    t = r.intersect(e);
+    if (t != -1){
+        Punto p = r.getPunto() + r.getDireccion() * t;
+        cout << p << endl;
+    } else cout << "No corta" << endl;
+     
     
 }
