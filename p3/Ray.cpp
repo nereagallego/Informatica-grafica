d68500553963951268d44fbcea589fc63a08a304
nerea <801950@unizar.es>
2022-10-23 03:22:09 -0700

    intersections

--- p3/Ray.cpp
+++ p3/Ray.cpp
@@ -5,12 +5,22 @@ Ray::Ray(const Direccion d, const Punto p){
     _punto = p;
 }
 
-Punto Ray::intersect(Plano p){
-    float t = -(p.getDistancia() + p.getNormal()*_punto)/(_direccion*p.getNormal()); 
-    Punto aux = _punto + _direccion*t;
-    return aux;
+float Ray::intersect(Plano p){
+    float denominador = _direccion*p.getNormal();
+    if (denominador == 0) return -1;
+    return -(p.getDistancia() + p.getNormal()*_punto)/(denominador); 
+    // Punto aux = _punto + _direccion*t;
+    // return aux;
 }
 
-Punto Ray::intersect(Esfera e){
-    
+float Ray::intersect(Esfera e){
+    Direccion aux = _punto - e.getCentro();
+    float radicando = (_direccion * aux * 2) * (_direccion * aux * 2) - 4 * _direccion.modulo() * _direccion.modulo() * (aux.modulo() * aux.modulo() - e.getRadio());
+    if (radicando < 0) return -1;
+    float delta = sqrt(radicando);
+    float r1 = (-(_direccion * aux * 2) - delta) / (2 * _direccion.modulo() * _direccion.modulo());
+    float r2 = (-(_direccion * aux * 2) + delta) / (2 * _direccion.modulo() * _direccion.modulo());
+    if (r1 > 0 && r2 > 0 && r1 < r2) return r1;
+    else if(r2 > 0) return r2;
+    else return -1;
 }
--- p3/Ray.h
+++ p3/Ray.h
@@ -6,6 +6,7 @@
 #include "../p1/Direccion.h"
 #include "../p1/Punto.h"
 #include "Plano.h"
+#include "Esfera.h"
 
 
 class Ray {
@@ -13,11 +14,29 @@ private:
     Direccion _direccion;
     Punto _punto;
 public:
-    
+    /**
+     * @brief Construct a new Ray object
+     * 
+     * @param d 
+     * @param p 
+     */
     Ray(const Direccion d, const Punto p);
 
-    Punto intersect(Plano p);
-    Punto intersect(Esfera e);
+    /**
+     * @brief Devuelve la distancia de intersección de un rayo con un plano
+     * 
+     * @param p plano con el que se calcula la intersección
+     * @return float 
+     */
+    float intersect(Plano p);
+
+    /**
+     * @brief Devuelve la distancia de intersección de un rayo con una esfera
+     * 
+     * @param e esfera con la que se calcula la intersección
+     * @return float 
+     */
+    float intersect(Esfera e);
 
     
 };
