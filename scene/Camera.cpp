99989bd29a25895e52d28c4203dad3617b999191
nerea <801950@unizar.es>
2022-11-06 14:14:25 -0800

    comment camera

--- scene/Camera.cpp
+++ scene/Camera.cpp
@@ -32,27 +32,17 @@ Direccion Camera::getF(){
     return _F;
 }
 
-// void Camera::getCuadricula(RGB vector[nPixels][nPixels]){
-//     vector = cuadricula;
-// }
-
-
 Imagen Camera::dibujar(){
-    // Matrix4 cambioBase1(_L,_U,_F,_O);
-    // Matrix4 cambioBase = cambioBase1.inversa();
     Imagen img(_nPixelsh, _nPixelsw,255,255);
     cout << _nPixelsw << " "  << _nPixelsh << endl;
     srand (time(NULL));
     for(int i = 0; i < _nPixelsh; i ++){
         for(int j = 0; j < _nPixelsw; j ++){
-       //     cout << img.getWidth() << " "  << img.getHeight() << endl;
-             float r1 = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/_anchura));
-             float r2 = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/_altura));
-             Punto centro(_referenciaPixel.getX()+r1+_anchura*j,_referenciaPixel.getY()-r2/2-_altura*i,_referenciaPixel.getZ());
-           // Punto centro(_referenciaPixel.getX()+_anchura/2+_anchura*j,_referenciaPixel.getY()-_altura/2-_altura*i,_referenciaPixel.getZ());
+            float r1 = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/_anchura));
+            float r2 = static_cast <float> (rand()) / (static_cast <float> (RAND_MAX/_altura));
+            Punto centro(_referenciaPixel.getX()+r1+_anchura*j,_referenciaPixel.getY()-r2/2-_altura*i,_referenciaPixel.getZ());
             cout << centro << endl;
             Ray rayo(centro-_O,_O);
-            //cout << rayo.getDireccion() << endl;
         
             float t = INFINITY;
             Intersect intersect;
@@ -60,9 +50,7 @@ Imagen Camera::dibujar(){
             RGB emision;
             bool corta = false;
             for(auto p : _primitives){
-            //    cout << p->getEmision();
-                intersect = p->intersect(rayo);
-            //    cout << " " << intersect._intersect << " " << intersect._t << endl; 
+                intersect = p->intersect(rayo); 
                 if(intersect._intersect && intersect._t < t && intersect._t > 0){
                     corta = true;
                     t = intersect._t;
@@ -71,7 +59,6 @@ Imagen Camera::dibujar(){
                 }
                 
             }
-        //    cout << endl;
             if(corta){
                 img._imagenHDR[i][j] = emision; 
             } else {
@@ -82,30 +69,6 @@ Imagen Camera::dibujar(){
     }
     return img;
 }
-
-// void Camera::save() const{
-//     vector<RGB> vect;
-//     Imagen img(nPixels,nPixels,255,255);
-//     float maxValue = 0;
-//     for(int i = 0; i < nPixels; i++){
-//         for(int j = 0; j < nPixels; j++){
-//             vect.push_back(cuadricula[i][j]);
-//             maxValue = max(maxValue,vect.back().getRed(), vect.back().getGreen(), vect.back().getBlue());
-//         }
-//     }
-    
-    
-   
-//     // for(int i = 0; i < nPixels; i++){
-//     //     for(int j = 0; j < nPixels; j++){
-           
-//     //         vect.push_back(cuadricula[i][j]);
-//     //     }
-//     // }
-//     img.setImagen(vect);
-//     img.exportFile("prueba.ppm");
-   
-// }
 
 void Camera::addPrimitive(shared_ptr<Primitive> p){
     _primitives.push_back(p);
--- scene/Camera.h
+++ scene/Camera.h
@@ -21,7 +21,6 @@ private:
     Direccion _F;
     Punto _O;
     int _nPixelsh, _nPixelsw;
- //   static RGB cuadricula[nPixels][nPixels];
     float _altura, _anchura;
     Punto _referenciaPixel;
 
@@ -30,18 +29,38 @@ private:
     float max(float a, float b, float c, float d) const;
 
 public:
+    /**
+     * @brief Construct a new Camera object
+     * Construye una cámara dados sus tres vectores de dirección el punto de 
+     * origen y la cantidad de píxeles que tiene en altura y anchura
+     * 
+     * @param l vector left de la cámara
+     * @param u vector up de la cámara
+     * @param f vector forward de la cámara
+     * @param o punto de origen de la cámara
+     * @param nPixelsh altura en píxeles
+     * @param nPixelsw anchura en píxeles
+     */
     Camera(Direccion l, Direccion u , Direccion f, Punto o, int nPixelsh, int nPixelsw);
 
     Direccion getL();
     Direccion getU();
     Direccion getF();
     Punto getO();
- //   void getCuadricula(RGB vector[nPixels][nPixels]);
 
+    /**
+     * @brief Devuelve la imagen con las primitivas que se hayan introducido 
+     * previamente
+     * 
+     * @return Imagen 
+     */
     Imagen dibujar();
 
-   // void save() const;
-
+    /**
+     * @brief Añade una nueva primitiva a la imagen
+     * 
+     * @param p primitiva a añadir
+     */
     void addPrimitive(shared_ptr<Primitive> p);
 };
 
