5e46dfdc4178cca188d29491c4fcdf05f22509c0
nerea <801950@unizar.es>
2022-11-06 13:59:35 -0800

    comment primitive

--- geometry/Primitive.h
+++ geometry/Primitive.h
@@ -7,11 +7,29 @@
 
 using namespace std;
 
+// abstract class 
 class Primitive{
+    // color del objeto
     RGB _emision;
 public:
+    /**
+     * @brief Construct a new Primitive object
+     * 
+     * @param emision color del objeto
+     */
     Primitive(RGB emision): _emision(emision){};
+
+    /**
+     * @brief Construct a new Primitive object
+     * Se le asigna color negro por defecto
+     * 
+     */
     Primitive(): _emision(RGB(0,0,0)) {}
+
+    /**
+     * @brief Destroy the Primitive object
+     * 
+     */
     ~Primitive() = default;
 
     void setEmision(RGB emision) {
@@ -22,6 +40,12 @@ public:
         return _emision;
     }
 
+    /**
+     * @brief virtual intersect function with a ray
+     * 
+     * @param r rayo con el que intersecta
+     * @return Intersect 
+     */
     virtual Intersect intersect(Ray r) = 0;
 };
 
