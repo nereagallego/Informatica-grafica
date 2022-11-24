d1fe133179e9ce4a15d8e844342ef18d04e7a55c
nerea <801950@unizar.es>
2022-11-24 13:42:48 -0800

    add finish condition

--- scene/Camera.cpp
+++ scene/Camera.cpp
@@ -146,6 +146,7 @@ RGB Camera::pathTracing(Ray r, int n,const int i){
     tuple<Direccion,RGB> tupla = cercano._emision.sample(r.getDireccion(), cercano._punto,cercano._normal);
     Direccion dirRay = get<0>(tupla);
     RGB color_BSDF = get<1>(tupla); 
+    if(color_BSDF.getRed() == 0 && color_BSDF.getGreen() == 0 && color_BSDF.getBlue() == 0) return RGB();
     
   //  contribucion = contribucion + color_BSDF * pathTracing(Ray(dirRay,cercano._punto),n++,i);
     contribucion = contribucion + color_BSDF *pathTracing(Ray(dirRay,cercano._punto),n++,i);
