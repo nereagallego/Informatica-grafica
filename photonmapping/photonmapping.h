885705b8cc9dd8eefa34a67eaece5cba54e445b5
nerea <801950@unizar.es>
2022-12-24 04:13:32 -0800

    doc

--- photonmapping/photonmapping.h
+++ photonmapping/photonmapping.h
@@ -19,17 +19,63 @@ class PhotonMapping{
     nn::KDTree<Photon,3,PhotonAxisPosition> fotonmap;
      mutex mtx;
     float radius; 
-public:
-    PhotonMapping(Camera cam, int n): _cam(cam), _numPhotons(n), radius(0.08f) {}
-
-    Imagen photonMapping();
-
+    
+    /**
+     * @brief reparte los fotones por la escena dada una cantidad de luces y un 
+     * número máximo de lanzamientos
+     * 
+     * @param l vector de luces
+     * @param n cantidad de lanzamientos de fotones
+     * @return vector<Photon>
+    */
     vector<Photon> ScatterPhotons(shared_ptr<Light> l, int n);
 
-    //Guarda los rebotes dado un foton
+    /**
+     * @brief guarda los rebotes del lanzamiento de un fotón
+     * 
+     * @param energía energía con la que entra el fotón 
+     * @param origen origen del anterior rebote
+     * @param dirRayo dirección con la que entra el fotón
+     * @return vector<Photon>
+    */
     vector<Photon> siguientesRebotes(RGB energia, Punto origen, Direccion dirRayo);
 
+
+    /**
+     * @brief Función auxliair para el cómputo del color de cada pixel
+     * 
+    */
     void work(ConcurrentQueue<pair<int,int>> &jobs, ConcurrentQueue<Pixel> &result, unsigned int nRays, int x, nn::KDTree<Photon,3,PhotonAxisPosition>& fotonmap);
 
+    /**
+     * @brief estimación de densidad de fotones en un punto dado
+     * 
+     * @param cercano intersección en la que se calcula la estimación de 
+     *                densidad
+     * @param rayo rayo entrante en la intersección
+     * @param v vector de fotones cercanos a la intersección
+    */
     RGB photonDensityStim(Intersect cercano, Ray rayo, const vector<const Photon*>& v);
+public:
+
+    /**
+     * @brief contruye un photonmapper dada una cámara y el número de fotones
+     * 
+     * @param cam parámetros de la cámara
+     * @param n numero de fotones
+    */
+    PhotonMapping(Camera cam, int n): _cam(cam), _numPhotons(n), radius(0.08f) {}
+
+    /**
+     * @brief return a image computed with photon mapping
+     * 
+     * @return Imagen
+    */
+    Imagen photonMapping();
+
+    
+
+    
+
+    
 };
