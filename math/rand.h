057851597abd0e610e5a7f1a207288b5e8ede7bd
nerea <801950@unizar.es>
2022-12-24 03:16:08 -0800

    doc

--- math/rand.h
+++ math/rand.h
@@ -3,6 +3,13 @@
 
 #include <random>
 namespace Rand{
+    /**
+     * @brief dvuelve un múmero aletatorio de tipo double entre fMin y fMax
+     * 
+     * @param fMin límite inferior
+     * @param fMax límite superior
+     * @return double
+    */
     double fRand(double fMin,double fMax);
 };
 
