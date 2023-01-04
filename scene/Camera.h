050648e979f4782e51787e5486d212c4105b9d13
nerea <801950@unizar.es>
2023-01-04 02:28:43 -0800

    pruebas

--- scene/Camera.h
+++ scene/Camera.h
@@ -37,7 +37,7 @@ private:
     int _nPixelsh, _nPixelsw;
     float _altura, _anchura;
     Punto _referenciaPixel;
-    int numRays = 1;
+    int numRays = 20;
     mutex mtx;
 
     uint32_t threads;
